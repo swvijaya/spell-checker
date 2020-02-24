cf7e5ef3272908b4bec3e34fdcb2880f09b77456
Swetha Vijayaraghavan <vijay.swetha@gmail.com>
2020-02-24 00:03:16 -0500

    Added factory and files for test cases

--- src/main/java/blmfltr/splchk/filter/StringBloomFilter.java
+++ src/main/java/blmfltr/splchk/filter/StringBloomFilter.java
@@ -1,44 +1,54 @@
 package blmfltr.splchk.filter;
 
 import blmfltr.splchk.exceptions.InitializationException;
-import org.springframework.stereotype.Service;
-
-import java.util.BitSet;
 
 /**
- * A bloom filter storage for a set of Strings
+ * Abstract class that has an implementation for calculating 8 different hashes for each string
  */
-public class StringBloomFilter{
+public abstract class StringBloomFilter implements BloomFilter {
 
-    private BitSet filterStore = null;
-    private int sizeOfData;
-    private float falsePositiveProbability;
-    private int noOfHashes;
-    private int sizeOfFilter;
-    private int wordsAdded = 0;
-    private static int MAX_HASHES = 8;
-    private static  int[] HASH_FACTOR = new int []{31, 41, 37, 53, 61,67,43,59};
+    //With  his implementation we are limiting the max. number of hashes to 8
+    static int MAX_HASHES = 8;
 
+    //Array of prime numbers to compute 8 different hashes
+    static  int[] HASH_FACTOR = new int []{31, 41, 37, 53, 61,67,43,59};
 
+    //Size of the bit array
+    int sizeOfFilter = 0;
 
+    //No of elements that are added to the array
+    int noOfElementsAdded = 0;
+
+    //No of hashes for a particular filter
+    int noOfHashes;
+
+    //total number of words that can be added to the filter
+    int sizeOfData;
 
 
     /**
+     * Constructor that computes the size of filter and the no of hashes based on number of strings that can be added
+     * and false positive probability.
      *
      * @param size
      * @param falsePositiveProbability
+     * @throws InitializationException
      */
-
-    public StringBloomFilter(int size, double falsePositiveProbability) throws InitializationException {
+    protected StringBloomFilter(int size, double falsePositiveProbability) throws InitializationException {
         if(size == 0 || falsePositiveProbability == 0){
             throw new InitializationException("Invalid input");
         }
         this.sizeOfData = size;
-        sizeOfFilter = computeSizeOfFilter(falsePositiveProbability);
-        filterStore = initFilter();
+        this.sizeOfFilter = computeSizeOfFilter(falsePositiveProbability);
         noOfHashes = computeNoOfHashes();
     }
 
+    /**
+     * Computes the number of Hashes required based on the formula:
+     * size of filter * ln(2) / no of strings to be added
+     *
+     * @return
+     */
     private int computeNoOfHashes() {
         int hashes = 0;
         if(sizeOfData > 0 ) {
@@ -47,6 +57,13 @@ public class StringBloomFilter{
         return hashes > MAX_HASHES ? MAX_HASHES : hashes;
     }
 
+    /**
+     * Computes the size of the bit array with the formular:
+     * Absolute value of (No of strings to be added * ln(false positive Probability)) /(ln 2)^2
+     *
+     * @param falsePositiveProbability
+     * @return
+     */
     private int computeSizeOfFilter( double falsePositiveProbability) {
         if(sizeOfData > 0){
             return (int) Math.abs((sizeOfData * Math.log(falsePositiveProbability) / (Math.log(2)*Math.log(2))));
@@ -54,38 +71,27 @@ public class StringBloomFilter{
         return 0;
     }
 
-    public void add(String input) throws InitializationException {
-        if(wordsAdded == sizeOfData){
-                throw new InitializationException("Filter limit reached");
+    /**
+     * Generates multiple hashes for the input String
+     *
+     * @param input
+     * @return
+     */
+    protected int[] hashes(String input){
+        int[] hashes = new int[noOfHashes];
+        for(int i=0;i<noOfHashes; i++){
+            hashes[i] = hash(input,i);
         }
-
-
-        for(int i=0; i< noOfHashes; i++){
-            int hash = hash(input, i);
-            filterStore.set(hash,true);
-        }
-
-        wordsAdded++;
-    }
-    public boolean mightContain(String input){
-        for(int i=0; i< noOfHashes; i++){
-            int hash = hash(input, i);
-            if(!filterStore.get(hash)){
-                return false;
-            }
-        }
-        return true;
+        return hashes;
     }
 
-    public int size(){
-        return sizeOfData;
-    }
-
-    public void clear(){
-        filterStore = initFilter();
-        int dataAdded = 0;
-    }
-
+    /**
+     * Generates hash based on a prime number
+     *
+     * @param input
+     * @param index
+     * @return
+     */
     private int hash(String input, int index){
         int hash = 0;
         int powers = 1;
@@ -96,20 +102,13 @@ public class StringBloomFilter{
         return hash;
     }
 
-    private BitSet initFilter(){
-        BitSet filter = new BitSet(sizeOfFilter);
-        if(filter != null && sizeOfFilter != 0){
-            filter.set(0,sizeOfFilter,false);
-        }
-        return filter;
+    /**
+     * Returns the number of elements added to the filter
+     *
+     * @return
+     */
+    public int getNoOfElementsAdded() {
+        return noOfElementsAdded;
     }
 
-
-    public int getWordsAdded() {
-        return wordsAdded;
-    }
-
-    public void setWordsAdded(int wordsAdded) {
-        this.wordsAdded = wordsAdded;
-    }
 }
